16bb1dcf6a625c0bb96533419e23cf848ccfe66f
JairoBonilla2004 <2c.bonilla.jairo0@gmail.com>
2025-12-08 21:10:40 -0500

    Estructura inicial del backend en springboot

---
+++ Codigo/Sprint 1/backend/mueblerix/src/main/java/ec/edu/espe/mueblerix/MueblerixApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.mueblerix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MueblerixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MueblerixApplication.class, args);
+	}
+
+}
---
+++ Codigo/Sprint 1/backend/mueblerix/src/test/java/ec/edu/espe/mueblerix/MueblerixApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.mueblerix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MueblerixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
